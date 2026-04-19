fc0e1b31ba2f559217704639f4d7d7c782163fe2
rekiciris <rekiciris@gmail.com>
2026-04-19 08:34:37 +0200

    update 4.7

--- uebung_4.7(preise).py
+++ uebung_4.7(preise).py
@@ -1,13 +1,23 @@
 preise = []
 einzelpreis = 1
+namen = []
 summe = 0
 
 while einzelpreis != 0:
-    einzelpreis = float(input("Gib den Preis ein: "))
-    if einzelpreis != 0:
-        preise.append(einzelpreis)
+    name = input("Was hast du gekauft? (oder stopp): ")
 
-for x in preise:
-    summe = summe + x
+    if name.lower() == "stopp":
+        break
 
-print(f"Gesamtsumme: {summe}")
+    einzelpreis = float(input(f"Was hat {name} gekostet? "))
+
+    namen.append(name)
+    preise.append(einzelpreis)
+
+print("\n---- KASSENBON ----")
+for i in range(len(preise)):
+    print(f"{namen[i]}: {preise[i]}")
+    summe += preise[i]
+
+print("-" * 20)
+print(f"Gesamtsumme: {summe}€")
